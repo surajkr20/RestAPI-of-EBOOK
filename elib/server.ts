529b0a5fc634ef9a955269c41127cb829a7c44e5
surajkr20 <suraj71308kumar@gmail.com>
2024-11-17 14:42:13 +0530

    setup express server

--- elib/server.ts
+++ elib/server.ts
@@ -1,1 +1,11 @@
-console.log("helo")
+import app from "./src/app";
+
+const startServer = () =>{
+    const PORT = process.env.PORT || 4000
+
+    app.listen(PORT, ()=>{
+        console.log(`app listen at PORT ${PORT}`)
+    })
+}
+
+startServer();
