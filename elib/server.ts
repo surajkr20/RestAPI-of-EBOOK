569f04891a13131be342b3fa8c6c13b716710a58
surajkr20 <suraj71308kumar@gmail.com>
2024-11-17 15:02:11 +0530

    setup config and env

--- elib/server.ts
+++ elib/server.ts
@@ -1,7 +1,9 @@
+
 import app from "./src/app";
+import {config} from './src/config/config'
 
 const startServer = () =>{
-    const PORT = process.env.PORT || 4000
+    const PORT = config.PORT || 4000
 
     app.listen(PORT, ()=>{
         console.log(`app listen at PORT ${PORT}`)
