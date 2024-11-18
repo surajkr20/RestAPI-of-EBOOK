50a88b6114088b32aad68b27536a14de7348b390
surajkr20 <suraj71308kumar@gmail.com>
2024-11-18 15:02:40 +0530

    user model added

--- elib/src/user/userController.ts
+++ elib/src/user/userController.ts
@@ -22,6 +22,11 @@ const createUser = async (
 
     // database call
     const user = await userModel.findOne({email})
+    // if user with same email exist, when this condition run
+    if(user){
+        const error = createHttpError(400, 'user already exist with this email')
+        return next(error)
+    }
 
     res.json({message: "user created"})
 };
