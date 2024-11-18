1b22acf5b49355aac5e8fbae549031bbbcc3c821
surajkr20 <suraj71308kumar@gmail.com>
2024-11-18 09:29:20 +0530

    basic modifications

--- elib/src/app.ts
+++ elib/src/app.ts
@@ -1,13 +1,10 @@
 
 import express, { NextFunction, Request, Response } from "express";
-import globalErrorHandler from "./middlewares/GlobalErrorHandler";
-import createHttpError from "http-errors";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app = express();
 
 app.get('/',(req, res, next)=>{
-    const error = createHttpError(400, "something went wrong")
-    throw error
     res.json({message: "welcome to elib api's"})
 })
 
